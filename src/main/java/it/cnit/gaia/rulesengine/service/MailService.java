3a52467ca5fc721adda3f326be5f31eaf0ea4e50
cuffaro <cuffaro.giovanni@gmail.com>
2017-08-23 16:50:22 +0200

    - Mailservice (with hard coded test credentials)

--- src/main/java/it/cnit/gaia/rulesengine/service/MailService.java
+++ src/main/java/it/cnit/gaia/rulesengine/service/MailService.java
@@ -1,5 +1,44 @@
 package it.cnit.gaia.rulesengine.service;
 
+import it.cnit.gaia.rulesengine.model.notification.GAIANotification;
+import net.sargue.mailgun.Configuration;
+import net.sargue.mailgun.Mail;
+import net.sargue.mailgun.Response;
+import net.sargue.mailgun.content.Body;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class MailService {
 	//https://github.com/sargue/mailgun
+	private Configuration configuration;
+
+	String api = "key-12c72acb4d1217567cf3f007290c303b";
+	String domain = "sandbox596bc5ced3644ff3a739a710d0c86d11.mailgun.org";
+	String frommail = "postmaster@sandbox596bc5ced3644ff3a739a710d0c86d11.mailgun.org";
+	String fromname = "Test account";
+	String tomail = "cuffaro.giovanni@gmail.com";
+
+	public MailService() {
+		configuration = new Configuration()
+				.domain(domain)
+				.apiKey(api)
+				.from(fromname, frommail);
+	}
+
+	public Response sendMailNotification() {
+		return Mail.using(configuration).to(tomail).subject("Prova").text("Hello").build().send();
+	}
+
+	public Response sendMailNotification(GAIANotification notification, String address){
+		String area = notification.getArea().getName();
+		String school = notification.getSchool().getName();
+		String suggestion = notification.getSuggestion();
+		Body body = Body.builder(configuration)
+						.h1(notification.getRuleName()+"-"+notification.getRuleClass())
+				.h2(school+"-"+area)
+				.strong(suggestion).build();
+		return Mail.using(configuration).content(body).to(address).subject("[GAIA Notification]").build().send();
+	}
+
 }
---
+++ src/test/java/service/MailServiceTest.java
@@ -0,0 +1,33 @@
+package service;
+
+import io.swagger.sparks.ApiException;
+import it.cnit.gaia.rulesengine.service.MailService;
+import net.sargue.mailgun.Response;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MailService.class})
+public class MailServiceTest {
+
+	String tomail = "cuffaro.giovanni@gmail.com";
+
+	@Autowired
+	MailService mailService;
+
+	@Before
+	public void setup(){
+	}
+
+	@Test
+	public void testSendingMail() throws ApiException {
+		Response response = mailService.sendMailNotification();
+	}
+
+}
