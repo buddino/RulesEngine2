9601d10f812e25f67804e4a1c6e49f4b2cd1b106
cuffaro <cuffaro.giovanni@gmail.com>
2017-10-18 16:36:11 +0200

    - Config from file - Naive limit in mail sent in 1 hour

--- src/main/java/it/cnit/gaia/rulesengine/service/MailService.java
+++ src/main/java/it/cnit/gaia/rulesengine/service/MailService.java
@@ -7,35 +7,66 @@ import net.sargue.mailgun.Response;
 import net.sargue.mailgun.content.Body;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+
 
 @Service
+@PropertySource("file:mailgun.properties")
 public class MailService {
 	//https://github.com/sargue/mailgun
 	Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-	//TODO
-	String api = "key-12c72acb4d1217567cf3f007290c303b";
-	String domain = "sandbox596bc5ced3644ff3a739a710d0c86d11.mailgun.org";
-	String frommail = "postmaster@sandbox596bc5ced3644ff3a739a710d0c86d11.mailgun.org";
-	String fromname = "Test account";
-	String tomail = "cuffaro.giovanni@gmail.com";
+
+	@Value("${mailgun.api_key}")
+	String api_key;
+	@Value("${mailgun.domain}")
+	String domain;
+	@Value("${mailgun.from_address}")
+	String frommail;
+	@Value("${mailgun.from_name}")
+	String fromname;
+
+	int mailSent = 0;
+	final int MAXMAIL = 30;  //Max mail per hour
+	Date latest = new Date();
+
 	private Configuration configuration;
 
-	public MailService() {
+	@PostConstruct
+	public void injectConfiguration(){
 		configuration = new Configuration()
 				.domain(domain)
-				.apiKey(api)
+				.apiKey(api_key)
 				.from(fromname, frommail);
 	}
 
-	public Response sendMailNotification() {
-		return Mail.using(configuration).to(tomail).subject("Prova").text("Hello").build().send();
+	public Response sendMailNotification(String address) {
+		return Mail.using(configuration).to(address).subject("Prova").text("Hello").build().send();
+	}
+
+	private boolean checkTimerange(){
+		Date now = new Date();
+		if (now.getTime() - latest.getTime() > 3600*1000){
+			//An hour has passed
+			latest = new Date();
+			mailSent = 0;
+			return false;
+		}
+		return true;
 	}
 
 	@Async
 	public Response sendMailNotification(GAIANotification notification, String address) {
+		if(checkTimerange()) {
+			if (++mailSent > MAXMAIL) {
+				LOGGER.error("MAX EMAIL LIMIT REACHED ("+MAXMAIL+"). Mail not sent to: "+address);
+			}
+		}
 		LOGGER.debug("Sending email to: " + address);
 		String area = notification.getArea().getName();
 		String school = notification.getSchool().getName();
@@ -52,6 +83,17 @@ public class MailService {
 		else
 			LOGGER.warn(response.responseMessage());
 		return response;
+	}
+
+	public Response sendMailNotificationMock(GAIANotification notification, String address) {
+		if(checkTimerange()) {
+			if (++mailSent > MAXMAIL) {
+				LOGGER.error("MAX EMAIL LIMIT REACHED ("+MAXMAIL+"). Mail not sent to: "+address);
+			}
+		}
+		LOGGER.info("Sending email to: " + address);
+		//Response response = Mail.using(configuration).content(body).to(address).subject("[GAIA Notification]").build().send();
+		return null;
 	}
 
 }
